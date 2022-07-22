39fe569c470910342ac9261445b74b9eadaa0483
mbsmani <46412901+mbsmani@users.noreply.github.com>
2022-07-22 11:38:47 +0530

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@ mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
 email = \"22ds1000119@student.onlinedegree.iitm.ac.in\"\n\
-">~/.streamlit/redentials.toml
+"> ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-">~/.streamlit/config.toml
+"> ~/.streamlit/config.toml
