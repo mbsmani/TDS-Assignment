d858b86ac6c2aaefc2cafc1fed08f9a4bb6d5ba6
mbsmani <mahesh.mithun406@gmail.com>
2022-07-22 10:56:23 +0530

    Added 3 supporting files

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"22ds1000119@student.onlinedegree.iitm.ac.in\"\n\
+">~/.streamlit/redentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+">~/.streamlit/config.toml
